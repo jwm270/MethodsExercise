981379677407310a73c6bc6860cfd1e972bd109a
Jimmy Moore <jwm270@gmail.com>
2025-04-18 21:45:37 -0500

    Part II done

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -17,9 +17,49 @@ namespace MethodsExercise
             
             Console.WriteLine($"{userName} approached the group and asked What is your favorite color? Arthur replied {favoriteColor}. {userName} asked what is your favorite movie? Lancelot replied {favoriteMovie}. Finally, {userName} asked Sir Gwayne, the Fearless, what is the airspeed velocity of an unladen swallow? Sir Gwayne replied {airSpeedVelocity} and that was the end of Sir Gwayne, the Fearless.");
         }
+
+        public static int Add(int numOne, int numTwo)
+        {
+            return numOne + numTwo;
+        }
+
+        public static int Substract(int numOne, int numTwo)
+        {
+            return numOne - numTwo;
+        }
+
+        public static int Multiply(int numOne, int numTwo)
+        {
+            return numOne * numTwo;
+        }
+
+        public static int Divide(int numOne, int numTwo)
+        {
+            return numOne / numTwo;
+        }
+
+        public static int Sum(params int[] numbers)
+
+        {
+            int sum = 0;
+
+            foreach(int number in numbers)
+            {
+            //sum = sum + number;   
+            sum += number;
+            }
+            return sum;
+        }
+
         static void Main(string[] args)
         {
-            Marstrip();
+            //Marstrip();
+            //Console.WriteLine(Add(10, 10));
+            //int addedNums = Add(10, 10);
+            //Console.WriteLine(addedNums);
+            //Console.WriteLine(Substract(10, 10));
+            //Console.WriteLine(Divide(numOne: 10, numTwo: 10));
+            //Console.WriteLine(Sum(10, 20));
         }
     }
 }
--- MethodsExercise/obj/Debug/net9.0/MethodsExercise.AssemblyInfo.cs
+++ MethodsExercise/obj/Debug/net9.0/MethodsExercise.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MethodsExercise")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+879bc51ef403450a0a853307a5e2bad9e738048e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3096a4e68ee9f3cc538b92a971b3d69d1af9fc1c")]
 [assembly: System.Reflection.AssemblyProductAttribute("MethodsExercise")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MethodsExercise")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
